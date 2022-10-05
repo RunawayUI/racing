697e39e143d40ffe6f49c173dc6fb6006851c1a7
RunawayUI <62843874+RunawayUI@users.noreply.github.com>
2022-10-05 09:51:27 +0300

    Add files via upload

---
+++ src/test/java/com/racing/webapprace/WebappraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.racing.webapprace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
